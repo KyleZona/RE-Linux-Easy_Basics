cfdecf9556e8f0a50aa247198e593723732ca2f5
KyleZona <kyeldridge@gmail.com>
2026-04-30 09:27:40 -0600

    Create .search_wesker2.sh
    Add script

---
+++ raccoon_forest/oswell_spencer_mansion/mainhall/.search_wesker2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+GAME_DIR="$(cd "$(dirname "$0")/../../.." && pwd)"
+source "$GAME_DIR/lib/typewriter-regular.sh"
+
+typewriter ""
+typewriter "    
+typewriter "        
+
+typewriter ""
+typewriter "        Barry: Find anything, Jill?"
+typewriter "        Jill: Nothing. What is this all about? I can't"
+typewriter "          figure it out at all."
+typewriter "        Barry: Beats me, too."
+typewriter "        Jill: Now it's Wesker's time to disappear! I don't"
+typewriter "          know what's going on."
+typewriter "        Barry: Let's search for him together."
+typewriter "           Maybe check the dining room again."      
+typewriter ""
