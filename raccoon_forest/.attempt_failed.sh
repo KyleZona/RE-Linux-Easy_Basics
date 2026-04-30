31088383416cd770f70a9273281fa736d7cf0af5
KyleZona <kyeldridge@gmail.com>
2026-04-29 20:00:10 -0600

    Create .attempt_failed.sh
    Add .attempt_failed.sh to play after failed password attempt into /oswell_spencer_mansion

---
+++ raccoon_forest/.attempt_failed.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+GAME_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+source "$GAME_DIR/lib/typewriter-dramatic.sh"
+
+typewriter ''
+typewriter '  Rabid dogs stopped a few feet away.'
+typewriter '  Growling, charging closer . . .
+sleep 0.5
+typewriter '  Jill looked back to the screen and read:'
+typewriter ''
+typewriter '    FAILED ATTEMPT.'
+typewriter '      ONLY ONE MORE ALLOWED . . .'
+typewriter ''
