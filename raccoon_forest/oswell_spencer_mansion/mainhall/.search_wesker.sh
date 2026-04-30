e2491a444de33cb59c63d8700be2340bfd1f97e2
KyleZona <kyeldridge@gmail.com>
2026-04-30 09:21:33 -0600

    Create .search_wesker.sh
    Add script and commands

---
+++ raccoon_forest/oswell_spencer_mansion/mainhall/.search_wesker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+GAME_DIR="$(cd "$(dirname "$0")/../../.." && pwd)"
+source "$GAME_DIR/lib/typewriter-regular.sh"
+
+typewriter ""
+typewriter "    Thunder roared again outside, heavy rain beat down on the"
+typewriter "    large windows that filled the. . . empty Main Hall."
+typewriter ""
+typewriter "        Barry: Wesker! Help me look for him, Jill."
+typewriter "          Try using command ls -la to see if"
+typewriter "          there's anything hidden in here..."
+typewriter ""
