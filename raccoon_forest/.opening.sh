61cbb60662fdb7fa52085ea61830ecd70eaad349
KyleZona <kyeldridge@gmail.com>
2026-04-29 19:31:56 -0600

    Create .opening.sh
    Added folder raccoon_forest with .opening.sh inside of it. Created .opening.sh with typewriter dialogue and scripting (loosely updated) from repository: Easy_Basics_w-Jill

---
+++ raccoon_forest/.opening.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+GAME_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+source "$GAME_DIR/lib/typewriter-regular.sh"
+
+typewriter ''
+typewriter '                — 1998 July — Raccoon Forest'
+typewriter ''
+sleep 0.5
+typewriter ''
+typewriter '  Chris Redfield: Alpha Team is flying around the forest'
+typewriter '  zone, situated in northwest Raccoon City, where we are'
+typewriter '  searching for the helicopter of our compatriots'
+typewriter '  Bravo Team, who disappeared during the middle of their'
+typewriter '  mission.'
+typewriter ''
+sleep 0.3
+typewriter ''
+typewriter '      Captain Wesker: Anyone seeing anything?'
+typewriter '      Barry Burton: Nothing yet.'
+typewriter ''
+sleep 0.5
+typewriter ''
+typewriter '  Chris: Bizarre murder cases have recently occurred in'
+typewriter '  Raccoon City. There are outlandish reports of families'
+typewriter '  being attacked by a group of about 10 people. Victims'
+typewriter '  were apparently eaten. Bravo Team went to the hideout'
+typewriter '  of the group, and disappeared.'
+typewriter ''
+sleep 0.3
+typewriter ''
+typewriter '      Jill: Look, Chris!'
+typewriter ''
+sleep 0.5
+typewriter ''
+typewriter "  Chris: It was Bravo Team's helicopter. Nobody was in it."
+typewriter '  But strangely, most of the equipment was still there.'
+typewriter '  However, we soon discovered why.'
+typewriter ''
+sleep 0.3
+typewriter ''
+typewriter "  Joseph's scream tore through the clearing."
+typewriter '  In his hand — a severed hand, still clutching a pistol.'
+typewriter ''
+sleep 0.3
+typewriter ''
+typewriter '  Rabid beasts jumped from the tall grass, hitting Joseph'
+typewriter '  like a wave. The other four S.T.A.R.S. members spun'
+typewriter "  to see Joseph's final moments."
+typewriter ''
+sleep 0.5
+typewriter ''
+typewriter '  Chris fired, snapping Jill out of pure shock. More rabid'
+typewriter '  . . . dogs? appeared. The team took off running back to'
+typewriter '  the helicopter gaining some distance.'
+typewriter ''
+sleep 0.3
+typewriter ''
+typewriter "      Chris: No! Don't go!"
+typewriter ''
+sleep 0.5
+typewriter ''
+typewriter '  The helicopter flew off into the dark horizon, leaving'
+typewriter '  the team standing there. The rabid dogs were getting'
+typewriter '  closer...'
+typewriter ''
+sleep 0.3
+typewriter ''
+typewriter '      Chris: Jill! Run for that house!'
+typewriter '  The four S.T.A.R.S. members sprinted through the thick'
+typewriter '  trees towards a soft glowing house. . .?'
+typewriter ''
+sleep 0.5
+typewriter ''
+typewriter "  It wasn't a house, it was a mansion. A perfect place to"
+typewriter "  find safety… Except the doors didn't open. Hardly budged."
+typewriter ''
+sleep 0.5
+typewriter ''
+typewriter '      cat ./Barry_Help.txt'
+typewriter ''
