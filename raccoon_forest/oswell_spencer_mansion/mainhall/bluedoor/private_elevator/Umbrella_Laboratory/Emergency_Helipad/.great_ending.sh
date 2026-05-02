9f3ab19d8f8a83a303323391fe3d134db591328f
KyleZona <263722125+KyleZona@users.noreply.github.com>
2026-05-02 13:09:34 -0600

    Create .great_ending.sh
    Add great_ending script with auto bash .thank_you.sh

---
+++ raccoon_forest/oswell_spencer_mansion/mainhall/bluedoor/private_elevator/Umbrella_Laboratory/Emergency_Helipad/.great_ending.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+GAME_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+source "$GAME_DIR/lib/typewriter-regular.sh"
+
+typewriter ""
+typewriter "    Jill planted her feet, and took aim. Bracing herself as the rocket launched.'
+typewriter "    The Tyrant stared at the missile curiously before exploding into a million'
+typewriter "    bits that rained down over Jill, Barry, And Chris.'
+typewriter ""
+typewriter "    Inside the helicopter, Jill rested her head on Chris's shoulder.'
+typewriter "    Barry loaded his Colt .45… Just in case...'
+typewriter ""
+typewriter "    In the distance, sirens from the self destruct sequence cut off.'
+typewriter "    Then, the entire mansion exploded. Taking with it any evidence of'
+typewriter "    trying to warn Raccoon_City or the world about what they had seen.'
+typewriter ""
+
+./.thank_you.sh
