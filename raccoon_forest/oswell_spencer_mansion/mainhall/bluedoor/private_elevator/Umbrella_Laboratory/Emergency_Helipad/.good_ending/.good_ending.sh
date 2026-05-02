c22b97b3291b7c30f248543dee5a3819d8c67b72
KyleZona <263722125+KyleZona@users.noreply.github.com>
2026-05-02 13:48:48 -0600

    Create .good_ending.sh
    add formatted script

---
+++ raccoon_forest/oswell_spencer_mansion/mainhall/bluedoor/private_elevator/Umbrella_Laboratory/Emergency_Helipad/.good_ending/.good_ending.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+GAME_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+source "$GAME_DIR/lib/typewriter-regular.sh"
+
+typewriter "" 
+typewriter "    JILL PLANTED HER FEET, AND TOOK AIM. BRACING HERSELF AS THE ROCKET LAUNCHED. THE"
+typewriter "    TYRANT STARED AT THE MISSILE CURIOUSLY BEFORE EXPLODING INTO A MILLION LITTLE"
+typewriter "    BITS THAT RAINED DOWN OVER JILL AND BARRY."
+typewriter ""
+typewriter "    JILL PULLED BARRY TO HIS FEET AND THEY RAN FOR THE HELICOPTER AS BRAD DESCENDED."
+typewriter "    INSIDE THE HELICOPTER, JILL SAT NEXT TO BARRY WATCHING THE MANSION IN THE DISTANCE."
+typewriter ""
+typewriter "        BARRY: What about. . . Chris?"
+typewriter "        JILL: Everything happened so fast..."
+typewriter "            I thought I saw some Holding_Cells, but. . ."
+typewriter ""
+typewriter "    IN THE DISTANCE, SIRENS FROM THE SELF DESTRUCT SEQUENCE CUT OUT."
+typewriter "    THEN, THE ENTIRE MANSION EXPLODED. TAKING WITH IT ANY EVIDENCE. . ."
+typewriter "    AND ANY HOPE OF EVER FINDING CHRIS REDFIELD."
+typewriter ""
+
+# bash .good_encouragement.sh 
