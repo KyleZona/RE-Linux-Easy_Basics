a3088feca518f178fe7f92921db7fb476944c118
KyleZona <263722125+KyleZona@users.noreply.github.com>
2026-05-02 13:50:33 -0600

    Create .good_encouragement.sh
    add formatted scripting

---
+++ raccoon_forest/oswell_spencer_mansion/mainhall/bluedoor/private_elevator/Umbrella_Laboratory/Emergency_Helipad/.good_ending/.good_encouragement.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+GAME_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+source "$GAME_DIR/lib/typewriter-regular.sh"
+
+typewriter ""
+typewriter "    CONGRATS -- YOU GOT THE GOOD ENDING."
+typewriter "    BUT WAIT! THERE'S A GREAT ENDING WAITING FOR YOU TO DISCOVER."
+typewriter ""
+typewriter "        . . .G O O D  L U C K !"
+typewriter ""
+
+# reset game files
+
+# exit and reset game
+exit
