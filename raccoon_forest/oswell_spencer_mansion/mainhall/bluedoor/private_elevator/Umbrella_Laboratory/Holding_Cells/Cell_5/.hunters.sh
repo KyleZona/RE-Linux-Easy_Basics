cae64c5437c8a4b05f135d75e8e4fe85405470b6
KyleZona <263722125+KyleZona@users.noreply.github.com>
2026-05-02 14:05:34 -0600

    Create .hunters.sh
    add formatted scripting

---
+++ raccoon_forest/oswell_spencer_mansion/mainhall/bluedoor/private_elevator/Umbrella_Laboratory/Holding_Cells/Cell_5/.hunters.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+GAME_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+source "$GAME_DIR/lib/typewriter-regular.sh"
+
+typewriter "" 
+typewriter "    The cell was filled with something that shouldn't exist."
+typewriter "" 
+typewriter "    Green scales. Yellow eyes that tracked Jill before she"
+typewriter "    could finish turning the handle. It leapt into the air."
+typewriter "" 
+typewriter "    The claws came down in a single clean arc. Jill didn't have"
+typewriter "    time to react. She didn't have time to scream."
+typewriter "" 
+typewriter "    Her head hit the floor before her body did."
+typewriter "" 
+
+# auto: ./.death.sh
