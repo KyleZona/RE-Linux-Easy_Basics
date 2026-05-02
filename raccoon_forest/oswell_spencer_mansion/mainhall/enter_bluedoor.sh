1315a47178b69bf3f46962b2a0a72fbec85d80f1
KyleZona <263722125+KyleZona@users.noreply.github.com>
2026-05-02 15:02:49 -0600

    Create enter_bluedoor.sh
    add formatted scripting

---
+++ raccoon_forest/oswell_spencer_mansion/mainhall/enter_bluedoor.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+GAME_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+source "$GAME_DIR/lib/typewriter-regular.sh"
+
+typewriter ""
+typewriter "        Barry: Geez, there's so many files."
+typewriter "          Be sure to read them carefully."
+typewriter ""
