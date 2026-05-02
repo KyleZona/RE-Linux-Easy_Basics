334c18d8f718088e6102852390cb03f92618bafc
KyleZona <263722125+KyleZona@users.noreply.github.com>
2026-05-02 14:01:29 -0600

    Create spiders.sh
    add formatted script

---
+++ raccoon_forest/oswell_spencer_mansion/mainhall/bluedoor/private_elevator/Umbrella_Laboratory/Holding_Cells/Cell_2/spiders.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+GAME_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+source "$GAME_DIR/lib/typewriter-regular.sh"
+
+typewriter ""
+typewriter "    The cell was filled with silence."
+typewriter ""
+typewriter "    Jill stepped inside. Empty. Just a chair, a desk, a"
+typewriter "    thin layer of... webs? Something shifted above her."
+typewriter ""
+typewriter "    She looked up. Eight eyes looked back."
+typewriter ""
+typewriter "    Legs the length of her arms wrapped around"
+typewriter "    her shoulders, her waist, her throat. The fangs found"
+typewriter "    the soft place under her jaw."
+typewriter ""
+typewriter "    Her body went limp, Barry raised his gun to aim"
+typewriter "    but the Spider shot him with... acid? Jill could not"
+typewriter "    move, couldn't process, only watch in horror as Barry"
+typewriter "    melted where he stood."
+typewriter ""
+typewriter "    Knowing she was next.
+typewriter ""
+
+# auto: ./.death.sh
