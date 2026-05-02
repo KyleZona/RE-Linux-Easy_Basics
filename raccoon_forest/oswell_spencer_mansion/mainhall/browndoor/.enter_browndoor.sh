9f2ed9d6469b8cc44858ba6986e961cf25140936
KyleZona <263722125+KyleZona@users.noreply.github.com>
2026-05-02 15:05:49 -0600

    Create .enter_browndoor.sh
    add formatted scripting

---
+++ raccoon_forest/oswell_spencer_mansion/mainhall/browndoor/.enter_browndoor.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+GAME_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+source "$GAME_DIR/lib/typewriter-regular.sh"
+
+typewriter ""
+typewriter "			Barry: Jill, you really are the master of unlocking."
+typewriter ""
+typewriter "			Jill: Thanks, Barry."
+typewriter ""
+typewriter "			Barry: Let's take a look around in here…"
+typewriter ""
