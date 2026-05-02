ef8214b3d0920ccc464f2ae66d196ab2e764f4fd
KyleZona <263722125+KyleZona@users.noreply.github.com>
2026-05-02 14:42:47 -0600

    Create .suspect_wesker.sh
    add formatted scripting

---
+++ raccoon_forest/oswell_spencer_mansion/mainhall/bluedoor/private_elevator/Umbrella_Laboratory/Research_Office/.suspect_wesker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+GAME_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+source "$GAME_DIR/lib/typewriter-dramatic.sh"
+
+typewriter ""
+typewriter "        Jill: Barry... this couldn't be..."
+typewriter "        Barry: We need to find Captain Albert Wesker immediately."
+typewriter ""
