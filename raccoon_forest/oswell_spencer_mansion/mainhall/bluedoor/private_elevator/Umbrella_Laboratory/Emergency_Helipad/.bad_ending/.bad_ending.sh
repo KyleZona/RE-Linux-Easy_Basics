fa7db47358d7b842961348643ebc744bc1afb1c4
KyleZona <263722125+KyleZona@users.noreply.github.com>
2026-05-02 13:41:36 -0600

    Create .bad_ending.sh
    add formatted scripting

---
+++ raccoon_forest/oswell_spencer_mansion/mainhall/bluedoor/private_elevator/Umbrella_Laboratory/Emergency_Helipad/.bad_ending/.bad_ending.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+GAME_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+source "$GAME_DIR/lib/typewriter-dramatic.sh"
+
+typewriter ""   
+typewriter "    JILL FIRED THE ROCKET LAUNCHER, WATCHING AS THE TYRANT RAISED ITS GIANT CLAW,"
+typewriter "    DEFLECTING THE MISSILE. IT SOARED INTO THE SKY, EXPLODING AGAINST THE HELICOPTER TAIL."
+typewriter "    EVERYTHING, INCLUDING BRAD, CAME CRASHING DOWN IN A BLAZE OVER JILL AND BARRY."
+typewriter "    SHE WATCHED AS THE TYRANT LEAPT INTO THE AIR AWAY FROM THE EMERGENCY_HELIPAD."
+typewriter "    TOWARDS. . . RACCOON_CITY. . ."
+typewriter ""
+typewriter "    IN HER FINAL MOMENTS, JILL HEARD THE SELF DESTRUCTION SEQUENCE."
+typewriter ""
+typewriter "    SELF DESTRUCTION WILL DETONATE IN. . ."
+typewriter "      ... three ..."
+typewriter "        ... two ..."
+typewriter "          ... o"
+typewriter ""
+
+# bash .death.sh
