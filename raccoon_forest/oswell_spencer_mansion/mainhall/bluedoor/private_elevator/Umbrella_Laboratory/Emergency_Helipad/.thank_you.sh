a42fb5622151b5a3b3166e71d3494fa7b2d2d66e
KyleZona <263722125+KyleZona@users.noreply.github.com>
2026-05-02 13:00:28 -0600

    Create .thank_you.sh
    Add scripting for thank you message and encouragement for next challenge

---
+++ raccoon_forest/oswell_spencer_mansion/mainhall/bluedoor/private_elevator/Umbrella_Laboratory/Emergency_Helipad/.thank_you.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+GAME_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+source "$GAME_DIR/lib/typewriter-regular.sh"
+
+typewriter ""
+typewriter "  Thank you for playing. I hope you've learned some basic linux commands"
+typewriter "  that can help you grow as you continue learning about this incredible"
+typewriter "  operating system."
+typewriter ""
+typewriter "  If you have been following the blog posts, every aspect has been"
+typewriter "  detailed providing thanks to the various sources in the community that"
+typewriter "  I relied on to make this."
+typewriter ""
+typewriter "  Iff you enjoyed this, please try the next challenge:"
+typewriter "  RE-L-More_Easy_Basics--where Jill and Barry split up!"
+typewriter ""
+typewriter "  Same premise, mix of new commands, and not as many hints"
+typewriter "  to help you survive the mansion this time either..."
+typewriter ""
+typewriter "  			. . .G O O D  L U C K !"
+
+#exit game and reset files
+exit
