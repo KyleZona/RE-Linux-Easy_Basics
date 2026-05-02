b1f4e928f83b4756d92cb3861d9fafae49ddac20
KyleZona <263722125+KyleZona@users.noreply.github.com>
2026-05-02 14:24:41 -0600

    Create .self-destruct-sequence.sh
    add formatted scripting

---
+++ lib/.self-destruct-sequence.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+GAME_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+source "$GAME_DIR/lib/typewriter-regular.sh"
+
+typewriter ""
+typewriter "    [Overhead: THIS FACILITY WILL DETONATE. ALL DOORS ARE NOW UNLOCKED.]"
+typewriter "    [Overhead: ALL PERSONNEL MUST EVACUATE IMMEDIATELY THROUGH]"
+typewriter "    [Overhead: DIRECTORY: Emergency_Helipad.]"
+typewriter ""
