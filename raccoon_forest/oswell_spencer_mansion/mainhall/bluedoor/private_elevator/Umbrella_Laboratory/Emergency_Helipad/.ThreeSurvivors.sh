ee472e8cfffaee07c8c55e51f768ffb17adb299f
KyleZona <263722125+KyleZona@users.noreply.github.com>
2026-05-02 13:17:31 -0600

    Update and rename ThreeSurvivors.sh to .ThreeSurvivors.sh
    add formatted script

---
+++ raccoon_forest/oswell_spencer_mansion/mainhall/bluedoor/private_elevator/Umbrella_Laboratory/Emergency_Helipad/.ThreeSurvivors.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+GAME_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+source "$GAME_DIR/lib/typewriter-regular.sh"
+
+typewriter ""
+typewriter "    
+typewriter "    Outside, the Emergency_Helipad was boxed in, hidden among the trees with
+typewriter "    the sounds of a helicopter in the distance. The sun was starting to rise,
+typewriter "    and Jill estimated that Brad was running out of fuel...
+typewriter ""
+typewriter "    In the corner, Jill spotted a red framed box with a bright orange flare
+typewriter "    gun inside. Grabbing it, she fired into the sky. Hope filled them both
+typewriter "    as the sound of the helicopter got louder.
+typewriter ""
+typewriter "    Then an explosion in the opposite corner.
+typewriter ""
+typewriter "    The sun, rising over the mountain, cast fresh light on the chunks of
+typewriter "    rubble that went flying among the large cloud of concrete dust.
+typewriter "    Jill, Barry, and Chris stared at the large black hole as the Tyrant leapt out
+typewriter "    and above their heads. Landing on his feet, his eyes locked on to Jill.
+typewriter "    Barry raised his gun to fire — nothing. He looked down at his Colt .45.
+typewriter ""
+typewriter "    		BARRY: Shit! I'm empty! Jill--
+typewriter ""
+typewriter "    Jill dove away as the Tyrant dashed between them with its claw
+typewriter "    jutting out. The Tyrant stood between them, looking from Chris, to Barry,
+typewriter "    and then to Jill.
+typewriter ""
+typewriter "    Deciding which one to annihilate first.
+typewriter ""
+typewriter "    		BRAD: USE THIS! KILL THAT MONSTER!
+typewriter ""
+typewriter "    Jill looked up to see Brad descending overhead. A large green box
+typewriter "    fell from the open doors. She recognized the box, which had a bright
+typewriter "    yellow sticker on all sides that read: TO FIRE ENTER: rocket_launcher.
+typewriter ""
+typewriter "    		BARRY: Hey! Big guy!
+typewriter "    		CHRIS: Over here!
+typewriter ""
+typewriter "    Jill watched as Barry and Chris distracted the Tyrant, and she knew that
+typewriter "    right now was her moment... She hurried to the box, opening it to pull out
+typewriter "    the big weapon.
+typewriter ""
+
+# Player prompted to fire — please enter the password: rocket_launcher
