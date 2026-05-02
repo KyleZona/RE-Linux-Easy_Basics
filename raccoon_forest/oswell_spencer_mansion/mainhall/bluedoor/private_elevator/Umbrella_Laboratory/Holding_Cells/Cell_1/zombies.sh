a7ddbcc4b32f5ff9f60e59cf1cc634aecce79473
KyleZona <263722125+KyleZona@users.noreply.github.com>
2026-05-02 13:58:39 -0600

    Create zombies.sh
    add formatted script

---
+++ raccoon_forest/oswell_spencer_mansion/mainhall/bluedoor/private_elevator/Umbrella_Laboratory/Holding_Cells/Cell_1/zombies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+GAME_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+source "$GAME_DIR/lib/typewriter-regular.sh"
+
+typewriter "" 
+typewriter "    The cell was filled with zombies.
+typewriter "" 
+typewriter "    A dozen of them. Maybe more.
+typewriter "    Overpowered Jill and Barry.
+typewriter "    Hands grabbed her hair.
+typewriter "    Teeth at his throat.
+typewriter ""
+
+# bash ./.death.sh
