f1d7d9dc97f4f46abcd7335e6a4dceeca8c9e7f9
KyleZona <263722125+KyleZona@users.noreply.github.com>
2026-05-02 14:49:29 -0600

    Create Enter_Umbrella2.sh
    add formatted scripting

---
+++ raccoon_forest/oswell_spencer_mansion/mainhall/bluedoor/private_elevator/Umbrella_Laboratory/Enter_Umbrella2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+GAME_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+source "$GAME_DIR/lib/typewriter-regular.sh"
+
+typewriter ""
+typewriter "        Jill: An Umbrella Laboratory?"
+typewriter ""
+typewriter "        Barry: That huge pharmaceutical corporation?"
+typewriter "          What were they doing down here?"
+typewriter ""
+typewriter "        Jill: Creating those monsters..."
+typewriter ""
+typewriter "        Barry: Let's look around."
+typewriter ""
