cd892604f7d60cc7de81ac732aef88ad5177ac28
KyleZona <263722125+KyleZona@users.noreply.github.com>
2026-05-02 14:47:07 -0600

    Create .Enter_Umbrella1.sh
    add formatted scripting

---
+++ raccoon_forest/oswell_spencer_mansion/mainhall/bluedoor/private_elevator/Umbrella_Laboratory/.Enter_Umbrella1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+GAME_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+source "$GAME_DIR/lib/typewriter-regular.sh"
+
+typewriter ""
+typewriter "        Barry: Where are we?"
+typewriter "            Use command pwd, Jill."
+typewriter ""
